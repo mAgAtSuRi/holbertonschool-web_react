6c8e4793c8152d89cd5cce3d391748b6bf2b3354
Tristan <tristan.latuilliere@gmail.com>
2026-01-15 11:06:23 +0100

    0 ft

--- TypeScript/task0/js/main.ts
+++ TypeScript/task0/js/main.ts
@@ -3,21 +3,34 @@ interface Student {
 	lastName: string,
 	age: number,
 	location: string,
-}
+};
 
 const student1: Student = {
 	firstName: "Tristan",
 	lastName: "Martinez",
 	age: 24,
 	location: "Dijon"
-}
+};
 const student2: Student = {
 	firstName: "Yashi",
 	lastName: "Shrivastava",
 	age: 26,
 	location: "India"
-}
-const studentList = [student1, student2]
-studentList.forEach(student => {
-	
+};
+
+const studentList: Student[] = [student1, student2]
+const table: HTMLTableElement = document.createElement('table');
+
+
+studentList.forEach((student: Student) => {
+	const row: HTMLTableRowElement = document.createElement('tr');
+
+  const firstnameCell: HTMLTableCellElement = document.createElement('td');
+  firstnameCell.textContent = student.firstName;
+
+  const localisationCell: HTMLTableCellElement = document.createElement('td');
+  localisationCell.textContent = student.location;
+
+  row.append(firstnameCell, localisationCell);
+  table.append(row);
 });
